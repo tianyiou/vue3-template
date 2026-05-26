60300afec3a9c4fc2b1d7034e397e24650d5d9ba
tianyiou <13018597316@163.com>
2026-05-26 14:35:08 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<script setup lang="ts">
+const a = 1
+const f1 = () => {
+  console.log('a', 'a')
+}
+</script>
+
+<style scoped></style>
