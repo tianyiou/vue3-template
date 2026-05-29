02173a5d16036baa290864edd4289710f709afaa
tianyiou <13018597316@163.com>
2026-05-29 22:54:18 +0800

    chore: 添加VSCode保存时Prettier自动格式化配置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-          <global-layout-config-provider>
-                    <router-view />
-          </global-layout-config-provider>
+  <global-layout-config-provider>
+    <router-view />
+  </global-layout-config-provider>
 </template>
 
 <script setup lang="ts"></script>
